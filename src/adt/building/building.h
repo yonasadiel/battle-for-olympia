d1371fd708593ffa5fee3b66d0db51ca0621c1f5
prisilamichelle <prisilamichelle@yahoo.com>
2017-11-13 12:28:03 +0700

    add building header file

--- src/adt/building/building.h
+++ src/adt/building/building.h
@@ -1,28 +1,19 @@
 #ifndef BUILDING_H
 #define BUILDING_H
 
-#include "player.h"
+#include "../player/player.h"
+#include "../point/point.h"
 
 typedef struct{
-	int row;
-	int col;
-}Location;
-
-typedef struct{
+	Point koordinat;
 	int maxHP; //maximum HP
 	int currHP;	//current HP
-}HP;
-
-typedef struct{
-	Location koordinat;
-	HP health;
 	Player owner; //pemilik building
 	int gold;
-	char jpetak; //jenis petak : normal, village, castle, tower
-	Player unitp; //unit yang sedang berada di dalam building
+	char type; //jenis petak : normal, village, castle, tower
 }Building;
 
-void CreateBuilding(Building *B, char jpetak, Player owner);
+void CreateBuilding(Building *B, char type, Player owner);
 /*	Membentuk sebuah building "kosong" yang siap diisi, dengan jenis petak dan pemilik tertentu.
 	I.S. Jenis petak dan pemilik building sudah terdefinisi.
 	F.S. Building B sesuai dengan definisi di atas terbentuk. */
