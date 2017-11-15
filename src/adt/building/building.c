c4ef05540ceefa245776dc8c1b4ef6b21380b7e2
prisilamichelle <prisilamichelle@yahoo.com>
2017-11-15 20:32:27 +0700

    add fungsi makebuilding

--- src/adt/building/building.c
+++ src/adt/building/building.c
@@ -12,9 +12,16 @@
 
 /* *** Selektor Building *** */
 /* #define BCoordinate(B) (B).Coordinate */
-/* #define BPlayer(B) (B).Player */
+/* #define BPlayer(B) (B).Owner */
 /* #define BType(B) (B).Type */
 
 void MakeBuilding(Building* B,Point C, Player O, char T) {
-  //
+/**
+ * I.S. B sembarang
+ * F.S. B terdefinisi di Coordinate C, dimiliki Player O
+ *      dan direpresentasikan huruf T
+ */
+  BCoordinate(*B) = C;
+	BPlayer(*B) = O;
+	BType(*B) = T;
 }
