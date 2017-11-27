da9b02486a744f4bd658b7371441911cadb9f80f
samuelsandi <samuelsandi1703@gmail.com>
2017-11-27 20:00:24 +0700

    driver unit,mesinkar,mesinkata

--- src/adt/unit/unit_driver.c
+++ src/adt/unit/unit_driver.c
@@ -1,12 +1,34 @@
 #include "unit.h"
 #include "../point/point.h"
+#include <stdio.h>
 
 int main(){
 	Unit U;
 	Point P;
+	int G;
+	char C;
 	MakePoint(1,2,&P);
 	CreateUnit(&U,'K',P);
 	PrintUnitName(U);
+	printf("\n");
 	printInfoUnit(U);
+	printf("\n");
+	C='A';
+	G=CheckGold(C);
+	printf("%d\n",G);
+	C='S';
+	G=CheckGold(C);
+	printf("%d\n",G);
+	C='W';
+	G=CheckGold(C);
+	printf("%d\n",G);
+	G=16;
+	PrintAvailRecruit(G);
+	G=17;
+	PrintAvailRecruit(G);
+	G=26;
+	PrintAvailRecruit(G);
+	G=41;
+	PrintAvailRecruit(G);
 	return 0;
 }
